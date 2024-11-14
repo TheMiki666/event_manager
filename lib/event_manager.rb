648ad5013ad989dd42dd6033212de07154db31c0
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-11-14 20:36:52 +0100

    DO ASSIGMENT PART 1
    Now we filter all the phones (and there are written at the bottom of the html generated document)

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -23,6 +23,24 @@ def legislators_by_zipcode(zip)
   end
 end
 
+#Asignment 1
+#Clean phone number
+def clean_phone_number(phone_number)
+  return "No valid phone" if phone_number.nil?
+
+  clean_phone=phone_number.delete(" ().-")
+  if clean_phone.length <10 || clean_phone.length>11
+    clean_phone="No valid phone"
+  elsif clean_phone.length==11
+    if clean_phone[0]=='1'
+      clean_phone=clean_phone[1..clean_phone.length-1]
+    else
+      clean_phone="No valid phone"
+    end
+  end
+  clean_phone
+end
+
 def save_thank_you_letter(id, form_letter)
     
   Dir.mkdir ('output') unless Dir.exist?('output')
@@ -52,6 +70,8 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone = clean_phone_number(row[:homephone])
 
   legislators = legislators_by_zipcode(zipcode)
 
